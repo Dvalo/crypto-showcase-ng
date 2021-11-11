aeae74a4d3b79ce5811dad5947573beb19127f14
Dvalo <guram.dvalishvili99@gmail.com>
2021-11-12 03:05:53 +0400

    Add searchbar functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { NgxEchartsModule } from 'ngx-echarts';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule,
     NgbModule,
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts'),
